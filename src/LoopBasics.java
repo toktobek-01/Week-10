c11bf62c1964c3902c734c1e30ef087e6c1bbeb1
toktobek-01 <toktobek.d@gmail.com>
2025-07-04 11:45:59 -0700

    week 10 arrays

--- src/LoopBasics.java
+++ src/LoopBasics.java
@@ -1,7 +1,7 @@
 public class LoopBasics {
     public static void main(String[] args) {
-        for(int i = 10; i >= 1; i--) {
-            System.out.println("Number: " + i);
+        for(int i = 1; i <= 10; i += 2) {
+            System.out.println("Only odd numbers: " + i);
         }
     }
 }

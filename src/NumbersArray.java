2dbf9551812c9c6d603daf1532264060519ec09c
toktobek-01 <toktobek.d@gmail.com>
2025-07-04 15:17:01 -0700

    week 10 arrays

--- src/NumbersArray.java
+++ src/NumbersArray.java
@@ -10,6 +10,9 @@ public class NumbersArray {
         int[] numbers = new int[count];
         int sum = 0;
 
+        int evenCount = 0;
+        int oddCount = 0;
+
 
         for (int i = 0; i < count; i++) {
             System.out.println("Enter number " + (i + 1) + ": ");
@@ -19,18 +22,24 @@ public class NumbersArray {
         int max = numbers[0];
         int min = numbers[0];
 
-        double average = (double) sum/count;
+        double average = (double) sum / count;
         System.out.println("You entered these numbers:");
         for (int i = 0; i < count; i++) {
             System.out.println(numbers[i]);
         }
 
-        for (int i = 1; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
             }
             if (numbers[i] < min) {
                 min = numbers[i];
+            }
+
+            if (numbers[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
             }
         }
 
@@ -38,7 +47,9 @@ public class NumbersArray {
         System.out.println("Average: " + average);
         System.out.println("Highest number: " + max);
         System.out.println("Lowest number: " + min);
+        System.out.println("Even numbers: " + evenCount);
+        System.out.println("Odd numbers: " + oddCount);
+
         sc.close();
     }
-
 }

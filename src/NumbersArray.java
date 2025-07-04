4ae57865e5de99415b43ed0e254892332766b4df
toktobek-01 <toktobek.d@gmail.com>
2025-07-04 15:31:36 -0700

    week 10 arrays

--- src/NumbersArray.java
+++ src/NumbersArray.java
@@ -9,38 +9,51 @@ public class NumbersArray {
 
         int[] numbers = new int[count];
         int sum = 0;
-
         int evenCount = 0;
         int oddCount = 0;
 
-
+        // Input numbers
         for (int i = 0; i < count; i++) {
             System.out.println("Enter number " + (i + 1) + ": ");
             numbers[i] = sc.nextInt();
             sum += numbers[i];
         }
+
+        // Now safe to initialize min/max
         int max = numbers[0];
         int min = numbers[0];
 
-        double average = (double) sum / count;
-        System.out.println("You entered these numbers:");
+        // Analyze numbers
         for (int i = 0; i < count; i++) {
-            System.out.println(numbers[i]);
+            if (numbers[i] > max) max = numbers[i];
+            if (numbers[i] < min) min = numbers[i];
+
+            if (numbers[i] % 2 == 0) evenCount++;
+            else oddCount++;
         }
 
+        // Sort numbers (ascending)
         for (int i = 0; i < count; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
+            for (int j = i + 1; j < count; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
             }
-            if (numbers[i] < min) {
-                min = numbers[i];
-            }
+        }
 
-            if (numbers[i] % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
-            }
+        // Output
+        double average = (double) sum / count;
+
+        System.out.println("You entered these numbers:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+        System.out.println("Sorted numbers in ascending order:");
+        for (int num : numbers) {
+            System.out.println(num);
         }
 
         System.out.println("Total sum: " + sum);

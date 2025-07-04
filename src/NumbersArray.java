55fd788975f564c39a8eb1333ae5feb355ff2d6b
toktobek-01 <toktobek.d@gmail.com>
2025-07-04 15:00:24 -0700

    week 10 arrays

--- src/NumbersArray.java
+++ src/NumbersArray.java
@@ -10,11 +10,14 @@ public class NumbersArray {
         int[] numbers = new int[count];
         int sum = 0;
 
+
         for (int i = 0; i < count; i++) {
             System.out.println("Enter number " + (i + 1) + ": ");
             numbers[i] = sc.nextInt();
             sum += numbers[i];
         }
+        int max = numbers[0];
+        int min = numbers[0];
 
         double average = (double) sum/count;
         System.out.println("You entered these numbers:");
@@ -22,9 +25,19 @@ public class NumbersArray {
             System.out.println(numbers[i]);
         }
 
+        for (int i = 1; i < count; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
         System.out.println("Total sum: " + sum);
         System.out.println("Average: " + average);
-
+        System.out.println("Highest number: " + max);
+        System.out.println("Lowest number: " + min);
         sc.close();
     }
 

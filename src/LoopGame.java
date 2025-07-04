d2dddbc331717c41a3b8a9c14973fa4c477b2c5f
toktobek-01 <toktobek.d@gmail.com>
2025-07-03 21:18:54 -0700

    week 10 arrays

--- src/LoopGame.java
+++ src/LoopGame.java
@@ -1,6 +1,6 @@
 public class LoopGame {
     public static void main(String[] args) {
-        for( int i = 1; i <=5; i++ ) {
+        for( int i = 10; i >= 1; i-- ) {
             System.out.println("Number: " + i );
         }
     }
